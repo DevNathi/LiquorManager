810ef1c88b9e8d0a3487c45d240a93171c7e7237
Nkosinathi Nkosi <nkosinathi.vodacom@gmail.com>
2019-12-12 14:10:03 +0200

    Add WPF Project With MVVM
    Add wpf project and configure it with Caliburn Micro for the MVVM framework. Aslo includes the Simple container DI system.

---
+++ LQMDesktopUI/ViewModels/ShellViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LQMDesktopUI.ViewModels
+{
+    public class ShellViewModel
+    {
+    }
+}
