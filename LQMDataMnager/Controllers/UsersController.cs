7ce53078872d9255ca15994a62aeadf8e3789294
Nkosinathi Nkosi <nkosinathi.vodacom@gmail.com>
2020-02-11 15:04:01 +0200

    Login View creation

---
+++ LQMDataMnager/Controllers/UsersController.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using LQMDataManager.Models;
+using Microsoft.AspNet.Identity;
+
+namespace LQMDataManager.Controllers
+{
+    [Authorize]
+    public class UsersController : ApiController
+    {
+        UserModel user = new UserModel();
+        List<UserModel> Users = new List<UserModel>(); 
+        public UsersController()
+        {
+            Users.Add(new UserModel { Id = 1, Username = "admin", Password = "admin123" });
+            Users.Add(new UserModel { Id = 2, Username = "mbuzi", Password = "mbuzi123" });
+            Users.Add(new UserModel { Id = 3, Username = "mbazo", Password = "mbazo123" });
+            Users.Add(new UserModel { Id = 4, Username = "muntu", Password = "muntu123" });
+        }
+        // GET: api/Users
+        public List<UserModel> Get()
+        {
+            return Users;
+        }
+
+        // GET: api/Users/5
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST: api/Users
+        public void Post([FromBody]string value)
+        {
+        }
+
+        // PUT: api/Users/5
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE: api/Users/5
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ LQMDesktopUI/ViewModels/LoginViewModel.cs
@@ -0,0 +1,37 @@
+﻿using Caliburn.Micro;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LQMDesktopUI.ViewModels
+{
+    class LoginViewModel : Screen
+    {
+        private string _username;
+        private string _password;
+        
+        public string UserName
+        {
+            get { return _username; }
+            set
+            {
+                _username = value;
+                NotifyOfPropertyChange(() => UserName);
+            }
+        }
+
+        public string Password
+        {
+            get { return _password; }
+            set
+            {
+                _password = value;
+                NotifyOfPropertyChange(() => Password);
+            }
+        }
+
+
+    }
+}
